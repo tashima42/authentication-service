afce0057a1cc03b6d65e27f4423ea3628ee9ff62
Pedro Tashima <pedrotashima42@gmail.com>
2021-08-30 09:25:10 -0300

    chore(refres token): update refresh token

--- src/use-cases/refreshToken.js
+++ src/use-cases/refreshToken.js
@@ -1,4 +1,4 @@
-export default function ({ keyRepository, userRepository, jwtHelper }) {
+export default function ({ Key, keyRepository, userRepository, jwtHelper }) {
   return async function ({ refreshToken }) {
     try {
       const opaqueKey = await keyRepository.findByKey(refreshToken)
@@ -9,10 +9,23 @@ export default function ({ keyRepository, userRepository, jwtHelper }) {
       if (!user) {
         throw new Error("Failed to find user") // TODO: add database error validation
       }
+      const userUuid = user.uuid
+      const key = Key.make({ userUuid })
+      const newOpaqueToken = await keyRepository.update({
+        uuid: opaqueKey.uuid,
+        userUuid,
+        key: key.key(),
+        name: key.name(),
+        opaque: true,
+      })
+      if (!newOpaqueToken) {
+        throw new Error("Failed to update refresh token") // TODO: add database error validation
+      }
       const token = jwtHelper.sign({ uuid: user.uuid, email: user.email })
       return {
         success: true,
-        token
+        token,
+        refreshToken: newOpaqueToken.key
       }
     } catch (error) {
       return {
